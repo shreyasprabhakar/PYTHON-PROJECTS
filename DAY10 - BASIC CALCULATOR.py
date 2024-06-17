1a6eb1a64560fb519d73089ac3d4901283271a5e
Shreyas Prabhakar <88151017+shreyasprabhakar@users.noreply.github.com>
2024-06-17 16:21:36 +0530

    Update DAY10 - BASIC CALCULATOR.py

--- DAY10 - BASIC CALCULATOR.py
+++ DAY10 - BASIC CALCULATOR.py
@@ -1,3 +1,28 @@
+logo_name = '''
+           _            _       _             
+          | |          | |     | |            
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ 
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |   
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|
+'''
+logo = '''
+ _____________________
+|  _________________  |
+| |                 | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+'''
+
 from replit import clear
 
 def add(n1, n2):
@@ -21,37 +46,38 @@ operations = {
 next_step = ""
 output = ""
 calc = True
+print(logo)
+print(logo_name)
 while calc:
   num1 = float(input("Enter your first number "))
   num2 = float(input("Enter your second number "))
-  
+
   #the loop function for continuing operating on the previous output
   def continue_loop():
     global next_step
     global output
     for operator in operations:
       print(operator)
-    
+
     req_op = input("Select an operation(Strictly do not input anything other than the above 4 options) ")
-    
+
     if req_op in operations:
       output = int(operations[req_op](num1, num2))
       print(output)
-    
+
     next_step = input("Enter 'continue' to operate further on this output. Enter 'new' to clear and start new calculation page.\nStrictly do not type anything but one of the two options given above.\n").lower()
 
   #initially calling the loop for the first calculation
   continue_loop()
-  
+
   while next_step == "continue":
     num1 = float(output)
     num2 = float(input("Enter the other number "))
     #calling the loop for continuing the operation with the previous output
     continue_loop()
-  
+
   if next_step == "new":
     clear()
-
 
 
 

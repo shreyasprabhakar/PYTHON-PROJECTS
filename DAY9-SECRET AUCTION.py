37642ef87427d57fb456e315805da0836f2a9406
Shreyas Prabhakar <88151017+shreyasprabhakar@users.noreply.github.com>
2024-06-17 09:31:57 +0530

    Add files via upload

---
+++ DAY9-SECRET AUCTION.py
@@ -0,0 +1,78 @@
+logo = '''
+⣿⣿⣿⣿⣿⣿⣿⠿⠟⠛⠛⠛⠛⠛⠛⠛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⢀⣀⣀⣠⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣆⣠⣤⡶⠶⠛⠛⠉⠁⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⢻⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠘⣷⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⠿⣯⠀⣿⡀⠀⠀⠀⠀⠀⠀⠀⢹⡆⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⠋⠉⠀⠀⠀⢻⡄⠸⣇⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⠟⢻⣿⣿⣿⣿⣿⣿⣿
+⣿⠀⢀⣀⣤⣤⣾⣷⣤⣿⡄⠀⠀⠀⠀⠀⠀⠀⢸⡏⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⠿⠿⠿⢿⣧⠀⠀⠀⠀⣀⣀⣤⡼⠿⠿⣧⠀⠀⠀⠀⣀⣴⣾⣿
+⣿⣿⣿⣿⣿⣿⣷⡄⢠⣤⣿⠶⠖⠚⠛⠉⠉⠀⠀⠀⠀⢻⡆⢀⣴⣾⣿⣿⣿⣿
+⣿⣿⣿⡿⠟⠋⠁⠀⠀⢿⡀⠀⠀⠀⠀⠀⢀⣀⣤⣤⠶⠾⠓⠀⠈⠻⣿⣿⣿⣿
+⣿⣿⣷⣶⣶⣦⣤⣤⠀⠸⣧⣠⣤⡶⠾⠛⠛⠉⠁⠀⠀⢠⣤⣤⣤⣤⣤⣽⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⢉⣉⠀⠀⠀⠀⣠⣦⣄⡀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣟⣠⣴⣾⣿⣿⡄⠀⣠⣾⣿⣿⣿⣿⣿⣶⣼⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+'''
+proj_name = '''
+██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    ████████╗ ██████╗     ████████╗██╗  ██╗███████╗    ███████╗███████╗ ██████╗██████╗ ███████╗████████╗     █████╗ ██╗   ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗
+██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗    ╚══██╔══╝██║  ██║██╔════╝    ██╔════╝██╔════╝██╔════╝██╔══██╗██╔════╝╚══██╔══╝    ██╔══██╗██║   ██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗         ██║   ██║   ██║       ██║   ███████║█████╗      ███████╗█████╗  ██║     ██████╔╝█████╗     ██║       ███████║██║   ██║██║        ██║   ██║██║   ██║██╔██╗ ██║
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝         ██║   ██║   ██║       ██║   ██╔══██║██╔══╝      ╚════██║██╔══╝  ██║     ██╔══██╗██╔══╝     ██║       ██╔══██║██║   ██║██║        ██║   ██║██║   ██║██║╚██╗██║
+╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝       ██║   ██║  ██║███████╗    ███████║███████╗╚██████╗██║  ██║███████╗   ██║       ██║  ██║╚██████╔╝╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║
+ ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝        ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚══════╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+                                                                                                                                                                                                                                  
+'''
+print(proj_name)
+
+#module for the clearing feature used in the project
+import clear
+
+#condition for keeping the auction running
+bidders = True
+
+#creating an empty list
+all_bidders = []
+
+#function for taking inputs
+def conduct_auction():
+  #making bidders global so that it can be modified from inside the function
+  global bidders
+  #asking for inputs
+  name = input("What is your name?\n")
+  bid = input("What's your bid?\n$")
+  people_left = input("Are there any other bidders left?\nType 'yes' or 'no'\n").lower()
+  if people_left == "yes":
+    ind_bidders = {}
+    ind_bidders["name"] = name
+    ind_bidders["bid"] = bid
+    all_bidders.append(ind_bidders)
+    #clearing the console
+    clear.clear()
+  elif people_left == "no":
+    clear.clear()
+    #making an empty list of bids
+    bids = []
+    for person in all_bidders:
+      bids.append(person["bid"])
+    #finding the highest bid
+    highest_bid = max(bids)
+    #finding the highest bidder
+    for bidder in all_bidders:
+      if bidder["bid"] == highest_bid:
+        winner = bidder["name"].upper()
+    #announcing the winner
+    print(logo)
+    print(f"{winner} is the winner")
+    bidders = False
+
+
+#main command to run the auction
+while bidders:
+  conduct_auction()
+    
+
+
+
+
